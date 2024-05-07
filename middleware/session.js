7ba03cb8f101d7ebd31eaef01bb164cc0b2b8534
Lars Nysom <lany@ucn.dk>
2024-05-07 12:09:55 +0200

    Added session cookie parameters

--- middleware/session.js
+++ middleware/session.js
@@ -4,5 +4,11 @@ const session = require('express-session');
 module.exports = session({
     secret: 'secret-key', 
     resave: false, 
-    saveUninitialized: false
+    saveUninitialized: false, 
+    name: 'sessionCookie',
+    cookie: {
+        secure: false, // if true: only transmit cookie over https, in prod, always activate this
+        httpOnly: true, // if true: prevents client side JS from reading the cookie
+        maxAge: 1000 * 60 * 30 // session max age in milliseconds
+    }
 });
